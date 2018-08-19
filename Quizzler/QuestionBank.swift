68d3b056710d93bfb14d00417fcb11a538c6bba7
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-08-19 09:52:50 -0700

    question bank created

--- Quizzler/QuestionBank.swift
+++ Quizzler/QuestionBank.swift
@@ -12,6 +12,10 @@ class QuestionBank {
     var list = [Question]()
     
     init() {
-        
+        list.append(Question(text: "Valentine\'s day is banned in Saudi Arabia.", correctAnswer: true))
+        list.append(Question(text: "A Slug\'s blood is green", correctAnswer: true))
+        list.append(Question(text: "Approximately one quarter of human bones are in the feet", correctAnswer: true))
+        list.append(Question(text: "In London, if you happen to die in the House of Pariement, you are technically entitled to a state funeral", correctAnswer: false))
+        list.append(Question(text: "It is illegal to piee in the Ocean in Portugal", correctAnswer: true))
     }
 }
