f3294730e67e25bdb50b1482fcfe768489f927d8
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-08-19 10:32:48 -0700

    added alert feature

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     
     //Place your instance variables here
     
+    let allQuestions = QuestionBank()
+    var pickedAnswer : Bool = false
+    var questionNumber : Int = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -20,34 +23,50 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateUI()
     }
-
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        checkAnswer()
+        nextQuestion()
     }
-    
     
     func updateUI() {
-      
+        questionLabel.text = allQuestions.list[questionNumber].questionText
     }
-    
 
     func nextQuestion() {
-        
+        if questionNumber < allQuestions.list.count - 1 {
+            questionNumber += 1
+            updateUI()
+        } else {
+            let alert = UIAlertController(title: "Awesome", message: "You've finished! Do you want to start over?", preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { (UIAlertAction) in
+                self.startOver()
+            })
+            alert.addAction(restartAction)
+            present(alert, animated: true, completion: nil)
+        }
     }
-    
     
     func checkAnswer() {
-        
+        let correctAnswer = allQuestions.list[questionNumber].answer
+        if correctAnswer == pickedAnswer {
+            print("correct guess")
+        } else {
+            print("wrong guess")
+        }
     }
-    
     
     func startOver() {
-       
+        print("starting over")
+        questionNumber = 0
+        updateUI()
     }
-    
-
     
 }
