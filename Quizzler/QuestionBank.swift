b60d998a174965fefe6b1a411bc41eac2dbfa286
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-08-19 09:46:49 -0700

    first commit

---
+++ Quizzler/QuestionBank.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionBank.swift
+//  Quizzler
+//
+//  Created by Kevin Lee on 8/19/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class QuestionBank {
+    var list = [Question]()
+    
+    init() {
+        
+    }
+}
---
+++ Quizzler/QuizBank.swift
@@ -0,0 +1,9 @@
+//
+//  QuizBank.swift
+//  Quizzler
+//
+//  Created by Kevin Lee on 8/19/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import Foundation
